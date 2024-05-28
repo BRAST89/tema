06150c5c31947dd34afece09ceb08aa5cdea8031
Steven Duran <100061375+BRAST89@users.noreply.github.com>
2024-05-27 19:38:44 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Document loaded');
+    // Puedes agregar más funcionalidades JavaScript aquí si es necesario.
+});
